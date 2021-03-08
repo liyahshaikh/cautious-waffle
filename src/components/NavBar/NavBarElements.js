3b65eec435af056eede0041aa23d9cba6c5fbae7
liyahshaikh <liyahshaikh965@gmail.com>
2021-03-09 00:39:55 +0530

    Started on the Nav bar elements

---
+++ src/components/NavBar/NavBarElements.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+ export const Nav = styled.nav`
+ background: #000;
+ height:100px;
+ display: flex;
+ 
+ font-weight: 700;
+
+ `
+ export const Img= styled.img`
+ height: 60px;
+width: 140px;
+padding-left: 20px;
+padding-top: 20px;
+align-self: left;
+ `
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Nav, Img} from "./NavBarElements"
+
+const NavBar = () => {
+    return (
+        <>
+            <Nav>
+                <Img src="WHITELOGO.png"></Img>
+            </Nav>
+        </>
+    )
+}
+
+export default NavBar
